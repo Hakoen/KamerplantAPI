f5161fceb4bcb51dfd3f65ce4dda34bc47686ae6
Hakoen <0949698@hr.nl>
2018-11-16 11:56:57 +0100

    Sessie Sluitert, log in en log uit endpoint gemaakt!
    Kort pittig is the new orange

--- Program.cs
+++ Program.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using sessie_Controllers;
 
 namespace kamerplanten_api
 {
@@ -14,6 +16,7 @@ namespace kamerplanten_api
     {
         public static void Main(string[] args)
         {
+            
             CreateWebHostBuilder(args).Build().Run();
         }
 
